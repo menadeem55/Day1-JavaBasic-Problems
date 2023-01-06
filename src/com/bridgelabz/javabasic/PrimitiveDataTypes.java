7381be42ff0bd3e8d1ac912907a93d4b84feb605
me.nadeem55@gmail.com <me.nadeem55@gmail.com>
2023-01-06 22:22:41 +0530

    Added: To Display All PrimitiveData Types

---
+++ src/com/bridgelabz/javabasic/PrimitiveDataTypes.java
@@ -0,0 +1,33 @@
+package com.bridgelabz.javabasic;
+
+public class PrimitiveDataTypes {
+	
+	//assign values to the variable
+			static byte b;
+			static short s;
+			static int i;
+			static long l;
+			static float f;
+			static double d;
+			static char ch;
+			static boolean bl;
+
+	public static void main(String[] args) {
+		
+		//printing message
+		System.out.println("Default values of Data Types: ");
+		System.out.println("Byte " + b);
+		System.out.println("Short: "+s);
+		System.out.println("Integer: "+i);
+		System.out.println("Long: "+l);
+		System.out.println("Float: "+f);
+		System.out.println("Double: "+d);
+		System.out.println("Character: "+ch);
+		System.out.println("Boolean: "+bl);
+		
+		
+		
+
+	}
+
+}
