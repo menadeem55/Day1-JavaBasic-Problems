ee28e30ee2d9c973c1e331160176337317b78534
me.nadeem55@gmail.com <me.nadeem55@gmail.com>
2023-01-07 15:21:15 +0530

    Added: CommandLine, StringEquals

---
+++ src/com/bridgelabz/javabasic/CheckEqualStrings.java
@@ -0,0 +1,18 @@
+package com.bridgelabz.javabasic;
+
+import java.util.Scanner;
+
+public class CheckEqualStrings {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter 1st String:");
+		String str1 = sc.next();
+		System.out.println("Enter 2nd String:");
+		String str2 = sc.next();
+		if (str1.equalsIgnoreCase(str2))
+			System.out.println("True");
+		else
+			System.out.println("False");
+	}
+
+}
