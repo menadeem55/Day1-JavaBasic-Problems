080f83316df2d36d0cd16a7a2971131c523ff145
me.nadeem55@gmail.com <me.nadeem55@gmail.com>
2023-01-07 22:55:36 +0530

    Added: logic to demonstrate the Static variable, methods and blocks

--- src/com/bridgelabz/javabasic/CommandLineName.java
+++ src/com/bridgelabz/javabasic/CommandLineName.java
@@ -1,12 +1,13 @@
 package com.bridgelabz.javabasic;
 
 public class CommandLineName {
-	// main method
 	public static void main(String[] args) {
 
-		// Display command line argument
-		String name = args[1];
-		System.out.println("The name of the user is: " + name);
+		System.out.println(args[0]);
+
+		System.out.println(args[1]);
+
+		System.out.println(args[2]);
 
 	}
 
